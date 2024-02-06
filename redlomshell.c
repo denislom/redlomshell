23dae46c16d22c4faf652697fe4c7c3a9caab5a9
Denis Lom <lom.denis@seznam.cz>
2024-02-06 22:46:33 +0100

    creating concept while not understanding so well the signals.

--- redlomshell.c
+++ redlomshell.c
@@ -6,39 +6,54 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 18:51:07 by dlom              #+#    #+#             */
-/*   Updated: 2024/02/04 00:58:23 by dlom             ###   ########.fr       */
+/*   Updated: 2024/02/06 22:45:29 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "redlomshell.h"
 
-int main(int argc, char **argv, char **env)
+void handle_sigint(int sig)
+{
+	// Print a new line and the prompt again, do not exit the shell
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void setup_signals(void)
+{
+	signal(SIGINT, handle_sigint); // Handle Ctrl-C
+	// Ignore SIGQUIT signal with Ctrl-\ signal(SIGQUIT, SIG_IGN);
+}
+
+int main(void)
 {
 	char *input;
-	char prompt[] = "minishell$ ";
+	t_command command; // Assume this is your command structure
 
-// Main loop
-	while (1) {
-		// Get input from user
-		input = readline(prompt);
+	setup_signals();
 
-		// Check for EOF (Ctrl+D)
-		if (!input) {
-			printf("\nExiting minishell.\n");
-			break;
-			}
+	while (1)
+	{
+		// Display prompt and wait for input
+		input = readline("minishell$ ");
+		if (!input)
+			break; // Exit if Ctrl-D is pressed
 
-		// Add input to history
-		if (ft_strlen(input) > 0) {
-			add_history(input);
-		}
+		if (*input)
+			add_history(input); // Only add non-empty commands to history
 
-        // Echo the input back to the user
-		ft_printf("You entered: %s\n", input);
+		// // Parse the input to fill your command structure
+		// parse_input(input, &command);
 
-		// Free the allocated memory
-		free(input);
+		// // Execute the command
+		// execute_command(&command);
+
+		// // Cleanup
+		// free(input);
+		// clear_command_structure(&command); // Assume this function clears your command structure
 	}
 
-	return 0;
+    // Perform any final cleanup if necessary
+    return 0;
 }
--- redlomshell.h
+++ redlomshell.h
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 00:18:00 by dlom              #+#    #+#             */
-/*   Updated: 2024/02/04 00:54:55 by dlom             ###   ########.fr       */
+/*   Updated: 2024/02/06 22:32:31 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,23 @@
 #include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <signal.h>
 # include "./libft/libft.h"
+
+typedef struct s_simple_command
+{
+	char **args; // Array of strings for arguments, including the command as args[0]
+} t_simple_command;
+
+typedef struct s_command
+{
+	t_simple_command **simple_commands; // Array of simple commands
+	char *infile;
+	char *outfile;
+	int append; // Boolean indicating if output redirection should append
+	int background; // Boolean indicating if the command should run in the background
+} t_command;
+
 
 #define MAX_INPUT_SIZE 1024
 
