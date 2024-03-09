7c86c2b35986331897f76a9556d434af87cf92ab
Denis Lom <lom.denis@seznam.cz>
2024-03-09 22:38:01 +0100

    constructors

---
+++ parser/construct.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   construct.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 19:41:16 by dlom              #+#    #+#             */
+/*   Updated: 2024/03/09 22:34:40 by dlom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../redlomshell.h"
+
+struct cmd	*exec_cmd(void)
+{
+	t_execcmd	*cmd;
+
+	cmd = malloc(sizeof(*cmd));
+	ft_memset(cmd, 0, sizeof(*cmd));
+	cmd->type = EXEC;
+	return (struct cmd*)cmd;
+}
+
+struct cmd	*redir_cmd(struct cmd *subcmd, char *file, char *efile, int mode, int fd)
+{
+	t_redircmd	*cmd;
+
+	cmd = malloc(sizeof(*cmd));
+	ft_memset(cmd, 0, sizeof(*cmd));
+	cmd->type = REDIR;
+	cmd->cmd = subcmd;
+	cmd->file = file;
+	cmd->efile = efile;
+	cmd->mode = mode;
+	cmd->fd = fd;
+	return (struct cmd*)cmd;
+}
+
+struct cmd	*pipe_cmd(struct cmd *left, struct cmd *right)
+{
+	t_pipecmd	*cmd;
+
+	cmd = malloc(sizeof(*cmd));
+	ft_memset(cmd, 0, sizeof(*cmd));
+	cmd->type = PIPE;
+	cmd->left = left;
+	cmd->right = right;
+	return (struct cmd*)cmd;
+}
+
+struct cmd	*list_cmd(struct cmd *left, struct cmd *right)
+{
+	t_listcmd	*cmd;
+
+	cmd = malloc(sizeof(*cmd));
+	ft_memset(cmd, 0, sizeof(*cmd));
+	cmd->type = LIST;
+	cmd->left = left;
+	cmd->right = right;
+	return (struct cmd*)cmd;
+}
+
+struct cmd	*back_cmd(struct cmd *subcmd)
+{
+	t_backcmd	*cmd;
+
+	cmd = malloc(sizeof(*cmd));
+	ft_memset(cmd, 0, sizeof(*cmd));
+	cmd->type = BACK;
+	cmd->cmd = subcmd;
+	return (struct cmd*)cmd;
+}
