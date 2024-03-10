c92a527002464e1b4c77097ae3de00b1669dffcd
Denis Lom <lom.denis@seznam.cz>
2024-03-10 22:30:24 +0100

    printf(bash: cd: %s: No such file or directory\n, (input+3));

--- redlomshell.c
+++ redlomshell.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 18:51:07 by dlom              #+#    #+#             */
-/*   Updated: 2024/03/10 22:20:36 by dlom             ###   ########.fr       */
+/*   Updated: 2024/03/10 22:29:24 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ int main(void)
 			// Chdir must be called by the parent, not the child
 			if (chdir(input + 3) == -1)
 			{
-				printf("cd should be performed");
+				printf("bash: cd: %s: No such file or directory\n", (input+3));
 			}
 			// if (chdir(input + 3) < 0)
 			// {
